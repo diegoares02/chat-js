fa6c9e70a2bfc3d70bf10149f6008de562db9709
diegoares02 <diegoares02@gmail.com>
2018-05-01 18:35:52 -0400

    Adding socket connection

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,19 @@
-io();
+$(function(){
+    const socket = io();
+
+    //getting DOM elements
+    var messageForm = $('#message-form');
+    var messageBox = $('#message');
+    var chat = $('#chat');
+
+    //eventos
+    messageForm.submit( e=>{
+        e.preventDefault();
+        socket.emit('send message',messageBox.val());
+        messageBox.val('');
+    })
+
+    socket.on('new message',function(data){
+        chat.append(data + '<br>');
+    })
+})
