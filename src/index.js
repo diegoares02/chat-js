a0f60bcc0730ea6330debf392b08c1da2e075a8f
diegoares02 <diegoares02@gmail.com>
2018-05-02 17:39:57 -0400

    Adding console log

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ const server = http.createServer(app);
 const io = socketio.listen(server);
 
 app.set('port',process.env.PORT || 3000);
+
 require('./sockets')(io);
 
+console.log(path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 server.listen(app.get('port'), () => {
-    console.log('server on port' + app.get('port'));
+    console.log('server on port ' + app.get('port'));
 })
